9f53fbf4e69d0bc6b311a0df0040648b70c79210
anansi-1 <anansisime7@gmail.com>
2025-05-18 03:30:44 +0300

    fix the applicant list screen and fetch api

--- lib/providers/applicants_provider.dart
+++ lib/providers/applicants_provider.dart
@@ -1,20 +1,16 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:dio/dio.dart';
 import '../models/applicant.dart';
-
-final dioProvider = Provider<Dio>((ref) => Dio());
+import 'dio_provider.dart';
 
 final applicantsProvider = FutureProvider.family<List<Applicant>, String>((
   ref,
   eventId,
 ) async {
-  final dio = ref.read(dioProvider);
+  final dio = ref.watch(dioProvider); // not read
 
   try {
-    final response = await dio.get(
-      'http://10.0.2.2:5500/api/event/$eventId/applicants/',
-    );
-
+    final response = await dio.get('/api/event/$eventId/applicants/');
     final data = response.data as List;
     return data.map((json) => Applicant.fromJson(json)).toList();
   } on DioException catch (e) {
--- lib/providers/dio_provider.dart
+++ lib/providers/dio_provider.dart
@@ -2,15 +2,14 @@ import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final dioProvider = Provider<Dio>((ref) {
-  final dio = Dio(
+  return Dio(
     BaseOptions(
-      baseUrl: 'http://10.0.2.2:5500', // your API base URL
+      baseUrl: 'http://10.0.2.2:5500',
       headers: {
         'Content-Type': 'application/json',
-        'Authorization':
-            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiamlyZW4gR3V0ZW1hIiwidXNlcmlkIjoyLCJyb2xlIjoib3JnYW5pemF0aW9uIiwiaWF0IjoxNzQ3NTEzNzU1LCJleHAiOjE3NDc2MDAxNTV9._fJe52pspYneulRc3MpxwX10kb2irPypkzrjJKgk1qM',
+         'Authorization':
+            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiamlyZW4gR3V0ZW1hIiwidXNlcmlkIjoyLCJyb2xlIjoib3JnYW5pemF0aW9uIiwiaWF0IjoxNzQ3NTEzNzU1LCJleHAiOjE3NDc2MDAxNTV9._fJe52pspYneulRc3MpxwX10kb2irPypkzrjJKgk1qM'
       },
     ),
   );
-  return dio;
 });
